70875de3bba7ee182e8b1b05cb27b6b4f87d82a2
Lucas Machiner <lucas.machiner@agileconnect.com.br>
2024-05-07 23:40:47 -0300

    final

--- funcao.php
+++ funcao.php
@@ -1,75 +1,70 @@
 <?php
 
-function Calcular($num1, $oper, $num2){
-    $res = '';
-    switch ($oper) {
+function Calcular($num1, $operation, $num2)
+{
+    $result = '';
+    switch ($operation) {
         case '+':
-            $res = $num1 + $num2;
+            $result = $num1 + $num2;
             break;
         case '-':
-            $res = $num1 - $num2;
+            $result = $num1 - $num2;
             break;
         case '*':
-            $res = $num1 * $num2;
+            $result = $num1 * $num2;
             break;
         case '/':
-            if($num2 != 0) {
-                $res = $num1 / $num2;
-            }
-            else{
+            if ($num2 != 0) {
+                $result = $num1 / $num2;
+            } else {
                 echo 'Não é possível dividir por 0';
             }
             break;
         case '^':
-            $res = 1;
-            for($i = 0; $i < $num2; $i++){
-                $res *= $num1;
+            $result = 1;
+            for ($i = 0; $i < $num2; $i++) {
+                $result *= $num1;
             }
             break;
         case '!':
-            $res = 1;
-            for($i = $num1; $i > 1; $i--){
-                $res *= $i;
+            $result = 1;
+            for ($i = $num1; $i > 1; $i--) {
+                $result *= $i;
             }
             break;
-        
+
         default:
-            
+
             break;
     }
 
-    return $res;
+    return $result;
 }
 
-function SalvarHistorico($operacao){
-    $_SESSION['historico'][] = "<li>$operacao</li>";
+function AddHistorico($equacao)
+{
+    $_SESSION['historico'][] = "<strong>$equacao</strong>";
 }
 
-function LimparSessoes(){
-    if (!isset($_SESSION)) session_start();
+function LimparHistorico()
+{
+    if (!isset($_SESSION)) {
+        session_start();
+    }
 
     $_SESSION['historico'] = array();
 }
 
-function Salvar($num1, $oper, $num2){
-    if($num1 && $oper) {
-        $num1 = isset($_GET['num1']) ? $_GET['num1'] : '';
-        $oper = isset($_GET['oper']) ? $_GET['oper'] : '';
-        $num2 = isset($_GET['num2']) ? $_GET['num2'] : '';
-        
-        $_SESSION['inputs'] = array('num1' => $num1, 'oper' => $oper, 'num2' => $num2);
-    }
-}
-
-function Pegar(){
+function Pegar()
+{
     $num1 = isset($_SESSION['inputs']['num1']) ? $_SESSION['inputs']['num1'] : '';
-    $oper = isset($_SESSION['inputs']['oper']) ? $_SESSION['inputs']['oper'] : '';
+    $operation = isset($_SESSION['inputs']['oper']) ? $_SESSION['inputs']['oper'] : '';
     $num2 = isset($_SESSION['inputs']['num2']) ? $_SESSION['inputs']['num2'] : '';
-        
-    if ($num1 !== '' && $oper !== '') {
-        $res = Calcular(intval($num1), $oper, intval($num2));
-        $texto = "$num1 $oper $num2 = $res";
-        SalvarHistorico($texto);
+
+    if ($num1 !== '' && $operation !== '') {
+        $result = Calcular((float) $num1, $operation, (float) $num2);
+        $texto = "$num1 $operation $num2 = $result";
+        AddHistorico($texto);
     }
-    
+
 }
--- index.php
+++ index.php
@@ -7,107 +7,99 @@ include 'funcao.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora</title>
-    <link rel="stylesheet" href="Style.css">
-</head>
-<body>
-   
-    <div class="calculadora">         
-        <h1>Calculadora</h1>  
 
-        <form method="get" action="">   
-            <!-- 1° Número -->
-            <input type="text" name="num1" placeholder="Primeiro numero">  
-            <!-- Operador -->
-            <select name="oper" required>                 
-                <option value="+">+</option>                 
-                <option value="-">-</option>                 
-                <option value=""></option>                 
-                <option value="/">/</option>                 
-                <option value="^">^</option>                 
-                <option value="!">!</option>             
-            </select> 
-            <!-- 2° Número -->
-            <input type="text" name="num2" placeholder="Segundo numero">             
-            <br>             
-            <input class="calcula" type="submit" value="Calcular">  
-            <div class="container">
-                <button name="salvar" class="btn-amarelo">Salvar</button>
-                <button name="pegar"class="btn-cinza">Pegar Valores</button>
-                <button name="m" value="memoria" class="btn-azul">M</button>
-                <button name="limpar" value="apagar"class="btn-azul">Apagar Historico</button>
-            </div>                    
-        </form>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calculadora</title>
+  <link rel="stylesheet" href="Style.css">
+</head>
+
+<body>
+
+  <div class="container">
+    <h1>Calculadora</h1>
+
+    <form method="get" action="">
+      <input type="text" name="num1" placeholder="Primeiro numero">
+      <select name="operation" required>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+        <option value="^">^</option>
+        <option value="!">!</option>
+      </select>
+      <input type="text" name="num2" placeholder="Segundo numero">
+
+      <input class="calcula" type="submit" value="Calcular">
+      <div class="container-button">
+        <button name="pegar" class="btn-cinza">Pegar Valores</button>
+        <button name="m" value="memoria" class="btn-black">M</button>
+        <button name="limpar" value="limpar" class="btn-danger">Apagar Historico</button>
+      </div>
+    </form>
 
 
     <?php
 
     $num1 = intval($_GET['num1']);
-    $oper = $_GET['oper'];
+    $operation = $_GET['operation'];
     $num2 = intval($_GET['num2']);
-    
-    if (isset($_GET['num1']) && isset($_GET['oper'])) {
 
+    if (isset($_GET['num1']) && isset($_GET['operation'])) {
 
-        $res = Calcular($num1, $oper, $num2); 
+      $result = Calcular($num1, $operation, $num2);
 
-        $texto = "";
+      $equacao = "";
 
-        if($num2 != null){
-            $texto = "$num1 $oper $num2 = $res";
-            echo "<div class=\"resposta\">
-                    $texto
-                </div>";
-        }
-        elseif($num2 == null && $num1 != null){
-            $texto = "$num1 $oper = $res";
-            echo "<div class=\"resposta\">
-                    $texto
-                </div>";
-        }
-        
-        if($texto != '') SalvarHistorico($texto);
-        
+      if ($num2 != null) {
+        $equacao = "$num1 $operation $num2 = $result";
+        echo '<div class="equacao">' . $equacao . "</div>";
+      }
+
+      if ($equacao != '')
+        AddHistorico($equacao);
     }
 
-    if(isset($_GET['salvar'])) Salvar($num1, $oper, $num2);
-    if (isset($_GET['pegar'])) Pegar();
+    if (isset($_GET['pegar']))
+      Pegar();
 
     if (isset($_GET['m'])) {
-        if (!isset($_SESSION['toggle'])) {
-            $_SESSION['toggle'] = false;
-        }
-    
-        if ($_SESSION['toggle']) Salvar($num1, $oper, $num2);
-        else {
-            Pegar();
-        }
-    
-        $_SESSION['toggle'] = !$_SESSION['toggle'];
+      if (!isset($_SESSION['toggle'])) {
+        $_SESSION['toggle'] = false;
+      }
+
+      if ($_SESSION['toggle']) {
+        Pegar();
+      }
+
+      $_SESSION['toggle'] = !$_SESSION['toggle'];
     }
 
-    if(isset($_GET['limpar']) && $_GET['limpar'] == "apagar") LimparSessoes();
+    if (isset($_GET['limpar']) && $_GET['limpar'] == "limpar")
+      LimparHistorico();
 
     ?>
 
-    <h1>Histórico</h1>
+
 
     <?php
 
-    if(isset($_SESSION['historico'])){
-        foreach ($_SESSION['historico'] as $operacao){
-            echo "<div class=\"historico\">
-                    $operacao
-                  </div>";
-        }
-    }
+    if (isset($_SESSION['historico'])) {
+      echo '<div class="historico">';
+      echo " <h2>Histórico</h2>";
+      foreach ($_SESSION['historico'] as $operacao) {
+        echo "<div>" . $operacao . "</div>";
 
-    echo "</div>";
+      }
+      echo "</div>";
+
+      echo "</div>";
+    }
 
     ?>
 
 </body>
+
 </html>
